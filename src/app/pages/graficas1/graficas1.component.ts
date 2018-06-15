85cc247198488e060546b27eda0dc61c9cf128e2
was <wascasla@hotmail.com>
2018-06-15 20:54:41 -0300

    algo falto

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -8,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class Graficas1Component implements OnInit {
 
    // Doughnut
-   public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-   public doughnutChartData: number[] = [350, 450, 100];
-   public doughnutChartType: string = 'doughnut';
+
 
    graficos: any = {
     'grafico1': {
